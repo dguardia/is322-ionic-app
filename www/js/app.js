d89b2c9ea9408d3ec179922ec37b0516742700b4
David Guardia <david.guardia@gmail.com>
2015-04-21 15:48:54 -0400

    working code  commit

--- www/js/app.js
+++ www/js/app.js
@@ -39,6 +39,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
+  .state('app.home', {
+    url: "/home",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/home.html"
+      }
+    }
+  })
+
   .state('app.browse', {
     url: "/browse",
     views: {
@@ -67,5 +76,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
