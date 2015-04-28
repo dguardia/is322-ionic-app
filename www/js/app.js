b1ca4a436522544f7ae3b1a682b7e52db4a24ba0
David Guardia <david.guardia@gmail.com>
2015-04-27 23:48:01 -0400

    adding new templates for the application

--- www/js/app.js
+++ www/js/app.js
@@ -6,75 +6,136 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.home', {
-    url: "/home",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/home.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
-});
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'AppCtrl'
+            })
+
+
+            .state('app.home', {
+                url: "/home",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/home.html"
+                    }
+                }
+            })
+
+            .state('app.dashboard', {
+                url: "/dashboard",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/dashboard.html"
+                    }
+                }
+            })
+            .state('app.invoices', {
+                url: "/invoices",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/invoices.html",
+                        controller: 'InvoicesCtrl'
+                    }
+                }
+            })
+            .state('app.invoice', {
+                url: "/invoices/:invoiceId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/invoice.html",
+                        controller: 'InvoiceCtrl'
+                    }
+                }
+            })
+            .state('app.addInvoice', {
+                url: "/addInvoice",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/addInvoice.html"}
+                }
+            })
+
+            .state('app.clients', {
+                url: "/clients",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/clients.html",
+                        controller: 'ClientsCtrl'
+                    }
+                }
+            })
+            .state('app.client', {
+                url: "/clients/:clientId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/client.html",
+                        controller: 'ClientCtrl'
+                    }
+                }
+            })
+
+            .state('app.addClient', {
+                url: "/addClient",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/addClient.html"}
+                }
+            })
+
+            .state('app.search', {
+                url: "/search",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/search.html"
+                    }
+                }
+            })
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/browse.html"
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: "/playlists",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlists.html",
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: "/playlists/:playlistId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlist.html",
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/home');
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,73 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('PlaylistsCtrl', function ($scope) {
+        $scope.playlists = [
+            {title: 'Reggae', id: 1},
+            {title: 'Chill', id: 2},
+            {title: 'Dubstep', id: 3},
+            {title: 'Indie', id: 4},
+            {title: 'Rap', id: 5},
+            {title: 'Cowbell', id: 6}
+        ];
+    })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
+    })
+
+    .controller('InvoicesCtrl', function ($scope) {
+        $scope.invoices = [
+            {title: 'Invoice 1', id: 1},
+            {title: 'Invoice 2', id: 2},
+            {title: 'Invoice 3', id: 3},
+            {title: 'Invoice 4', id: 4},
+            {title: 'Invoice 5', id: 5},
+            {title: 'Invoice 6', id: 6}
+        ]
+    })
+    .controller('InvoiceCtrl', function ($scope, $stateParams) {
+    })
+    .controller('ClientsCtrl', function ($scope) {
+        $scope.clients = [
+            {title: 'Client 1', id: 1},
+            {title: 'Client 2', id: 2},
+            {title: 'Client 3', id: 3},
+            {title: 'Client 4', id: 4},
+            {title: 'Client 5', id: 5},
+            {title: 'Client 6', id: 6}
+        ]
+    })
+    .controller('ClientCtrl', function ($scope, $stateParams) {
+    });
